4ad9457dedd4f46fd062ed704b4df30d311a07d0
anil <anillcosar@gmail.com>
2018-04-16 01:30:13 +0300

    Test error fixed

--- src/test/java/me/cosean/chat/ChatApplicationTests.java
+++ src/test/java/me/cosean/chat/ChatApplicationTests.java
@@ -2,11 +2,13 @@ package me.cosean.chat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ChatApplicationTests {
 
 	@Test
